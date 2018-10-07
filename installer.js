6296f3f49629aa56d102f8d57d07dea268f28a77
Bowser65 <contact.bowser65@gmail.com>
2018-10-07 12:57:16 +0200

    Don't spam console

--- installer.js
+++ installer.js
@@ -2,21 +2,20 @@ const { execSync } = require('child_process')
 const { migrator } = require('./migrations/_migrator')
 
 console.log('Union installer v1.0.0')
-
 console.log('Migrating database...')
 migrator().then(() => {
   console.log('Downloading web interface...')
-  execSync('rm -rf union-react')
-  execSync('git clone https://github.com/Union-Chat/union-react')
+  execSync('rm -rf union-react', { stdio: [null, null, null] })
+  execSync('git clone https://github.com/Union-Chat/union-react', { stdio: [null, null, null] })
 
   console.log('Building web interface...')
-  execSync('cd union-react && yarn && yarn run prod && cd ..')
+  execSync('cd union-react && yarn && yarn run prod && cd ..', { stdio: [null, null, null] })
 
   console.log('Cleaning up...')
-  execSync('rm -rf dist')
-  execSync('mv union-react/index.html index.html')
-  execSync('mv union-react/dist dist')
-  execSync('rm -rf union-react')
+  execSync('rm -rf dist', { stdio: [null, null, null] })
+  execSync('mv union-react/index.html index.html', { stdio: [null, null, null] })
+  execSync('mv union-react/dist dist', { stdio: [null, null, null] })
+  execSync('rm -rf union-react', { stdio: [null, null, null] })
 
   console.log('Success! Union is ready to work. Just do `yarn run start`')
   // Manually exit because rethink is annoying
