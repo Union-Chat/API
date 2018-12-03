0cbd2c6aee1cae516f0301f610868b7df5930a86
Devoxin <lukel9914@gmail.com>
2018-12-03 19:35:03 +0000

    chrome has the big gay

--- Server.js
+++ Server.js
@@ -76,7 +76,7 @@ app.use(allowCORS);
 
 function allowCORS (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 }
 
