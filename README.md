d073aced8856939702550a87b76286ecfe931427
Bowser65 <contact.bowser65@gmail.com>
2018-12-10 17:44:41 +0100

    Custom root credentials

--- README.md
+++ README.md
@@ -4,13 +4,12 @@ A light chat service
 ## Installation
 
  - `git clone https://github.com/Union-Chat/Union-Server` - Download Union-Server
- - `yarn run setup` - Build the server & the UI, setup the database
+ - `yarn run setup` - Build the server & the UI, setup the database<br>
+During this step, the installer will ask you the credentials for administrator
+account. By default, it'll be `root` and `root` but you can change is as you
+want. Discriminator will always be `0001` 
  - Create a Configuration.json with your custom settings
  - `yarn run start` - Start the app!
-
-By default a root user with admin perms will be created
-<br/>Username: `root#0001`
-<br/>Password: `root` (you should change it as soon as possible)
 
 ## Updating
 
--- migrations/2.root.js
+++ migrations/2.root.js
@@ -1,17 +1,41 @@
 const FlakeId = require('flakeid');
 const { hash } = require('bcrypt');
+const prompt = require('prompt');
+
 const idGenerator = new FlakeId({
   timeOffset: (2018 - 1970) * 31536000 * 1000
 });
 
-module.exports = async (r, dbName) => {
-  await r.db(dbName).table('users').insert({
-    id: idGenerator.gen(),
-    username: 'root',
-    discriminator: '0001',
-    password: await hash('root', 10),
-    servers: [],
-    online: false,
-    admin: true
+module.exports = (r, dbName) => {
+  return new Promise(res => {
+    prompt.start();
+    prompt.message = '';
+    prompt.delimiter = '';
+    console.log(' > Choose credentials for root account');
+    prompt.get({
+      properties: {
+        username: {
+          pattern: /^[a-zA-Z0-9\s\-_]+$/,
+          default: 'root'
+        },
+        password: {
+          default: 'root',
+          hidden: true,
+          replace: '*'
+        }
+      }
+    }, async (err, results) => {
+      await r.db(dbName).table('users').insert({
+        id: idGenerator.gen(),
+        username: results.username,
+        discriminator: '0001',
+        password: await hash(results.password, 10),
+        servers: [],
+        online: false,
+        admin: true
+      });
+      prompt.stop();
+      res();
+    });
   });
 };
